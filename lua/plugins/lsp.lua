d24aeae8c73f36967923f810296f1a0f938a0fd2
amdlemos <amdlemos@gmail.com>
2025-02-18 15:51:19 -0300

    feat(lsp): add keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -30,6 +30,18 @@ return {
 			require("plugins.lsp.phpactor")
 			require("plugins.lsp.psalm")
 			require("plugins.lsp.phan")
+
+			-- keymaps for help, go to definition, code actions & other
+			local keymap = vim.keymap
+			local buf = vim.lsp.buf
+			keymap.set("n", "K", buf.hover, { desc = "LSP Hover" })
+			keymap.set("n", "gd", buf.definition, { desc = "LSP Definition" })
+			keymap.set("n", "gD", buf.declaration, { desc = "LSP Declaration" })
+			keymap.set("n", "gi", buf.implementation, { desc = "LSP implementation" })
+			keymap.set("n", "gr", buf.references, { desc = "LSP References" })
+			keymap.set("n", "rn", buf.rename, { desc = "LSP Rename" })
+			keymap.set("n", "D", buf.type_definition, { desc = "LSP Definition" })
+			keymap.set({ "n", "v" }, "<leader>ca", buf.code_action, { desc = "LSP Code Action" })
 		end,
 	},
 }
