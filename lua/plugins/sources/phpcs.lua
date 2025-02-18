7cf40e86312371fccd45d000f0e536a55675d019
amdlemos <amdlemos@gmail.com>
2025-02-18 15:49:43 -0300

    fix(none-ls): remove condition to prevent multiple diagnostics executions

--- lua/plugins/sources/phpcs.lua
+++ lua/plugins/sources/phpcs.lua
@@ -4,7 +4,4 @@ local builtins = null_ls.builtins
 return builtins.diagnostics.phpcs.with({
   prefer_local = "vendor/bin",
   method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
-  condition = function(utils)
-    return utils.root_has_file("phpcs.xml")
-  end,
 })
--- lua/plugins/sources/phpmd.lua
+++ lua/plugins/sources/phpmd.lua
@@ -5,7 +5,4 @@ return builtins.diagnostics.phpmd.with({
   prefer_local = "vendor/bin",
   extra_args = { "phpmd.xml" },
   method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
-  condition = function(utils)
-    return utils.root_has_file("phpmd.xml")
-  end,
 })
