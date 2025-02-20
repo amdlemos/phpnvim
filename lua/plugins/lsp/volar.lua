85c6766d2466883c56ee2a45c0b42b54ab235042
amdlemos <amdlemos@gmail.com>
2025-02-19 23:15:07 -0300

    fix: adjust Volar configuration for proper functionality

--- lua/plugins/lsp/volar.lua
+++ lua/plugins/lsp/volar.lua
@@ -1,18 +1,51 @@
 local lspconfig = require("lspconfig")
 
-local project_root = vim.fn.getcwd()
-local vue_plugin_path = project_root .. "/node_modules/@vue/"
+-- local project_root = vim.fn.getcwd()
+-- local vue_plugin_path = project_root .. "/node_modules/@vue/language-server/"
+local typescript_path = vim.fn.getcwd() .. "/node_modules"
 
 lspconfig.volar.setup({
+	on_attach = function(client)
+		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentRangeFormattingProvider = false
+		-- client.server_capabilities.semanticTokensProvider = false
+	end,
+	-- cmd = { vue_plugin_path .. "vue-language-server", "--stdio" },
 	capabilities = require("blink.cmp").get_lsp_capabilities(),
 	init_options = {
-		plugins = { -- I think this was my breakthrough that made it work
-			{
-				name = "@vue/typescript-plugin",
-				location = vue_plugin_path .. "language-server",
-				languages = { "vue" },
-			},
+		vue = {
+			hybridMode = false,
 		},
+		typescript = {
+			tsdk = typescript_path .. "/typescript/lib", -- Certifique-se de que o TypeScript do projeto está sendo usado
+		},
+		-- completion = {
+		-- 	autoImport = true,
+		-- 	useScaffoldSnippets = true,
+		-- },
 	},
-	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+	-- settings = {
+	-- 	typescript = {
+	-- 		inlayHints = {
+	-- 			enumMemberValues = {
+	-- 				enabled = true,
+	-- 			},
+	-- 			functionLikeReturnTypes = {
+	-- 				enabled = true,
+	-- 			},
+	-- 			propertyDeclarationTypes = {
+	-- 				enabled = true,
+	-- 			},
+	-- 			parameterTypes = {
+	-- 				enabled = true,
+	-- 				suppressWhenArgumentMatchesName = true,
+	-- 			},
+	-- 			variableTypes = {
+	-- 				enabled = true,
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
+
+	filetypes = { "typescript", "javascript", "vue", "json" },
 })
