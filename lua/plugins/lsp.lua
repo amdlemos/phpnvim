bd5b58a05a64fb738a5f34981a48c97be75c3823
amdlemos <amdlemos@gmail.com>
2025-02-19 23:32:31 -0300

    chore: remove keymap and add diagnostic config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,18 +26,28 @@ return {
 		debug = true,
 		enabled = true,
 		config = function()
+			vim.diagnostic.config({
+				virtual_text = false,
+				signs = true,
+				underline = true,
+				severity_sort = true,
+				update_in_insert = false,
+			})
+
 			require("plugins.lsp.intelephense")
-			-- require("plugins.lsp.phpactor")
 			require("plugins.lsp.psalm")
 			require("plugins.lsp.phan")
 			require("plugins.lsp.emmet_language_server")
-			-- require("plugins.lsp.biome")
 			require("plugins.lsp.html")
 			require("plugins.lsp.eslint")
 			require("plugins.lsp.astro")
+			require("plugins.lsp.volar")
+
+			-- Disabled
 			-- require("plugins.lsp.ts_ls")
 			-- require("plugins.lsp.vtsls")
-			require("plugins.lsp.volar")
+			-- require("plugins.lsp.biome")
+			-- require("plugins.lsp.phpactor")
 
 			local keymap = vim.keymap
 			local buf = vim.lsp.buf
@@ -47,7 +57,7 @@ return {
 			keymap.set("n", "gi", buf.implementation, { desc = "LSP implementation" })
 			keymap.set("n", "gr", buf.references, { desc = "LSP References" })
 			keymap.set("n", "rn", buf.rename, { desc = "LSP Rename" })
-			keymap.set("n", "D", buf.type_definition, { desc = "LSP Definition" })
+			-- keymap.set("n", "D", buf.type_definition, { desc = "LSP Definition" })
 			keymap.set({ "n", "v" }, "<leader>ca", buf.code_action, { desc = "LSP Code Action" })
 		end,
 	},
