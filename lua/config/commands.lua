73ca659648eaf76e46d64e7456119d6d7f6afa0c
amdlemos <amdlemos@gmail.com>
2025-02-22 00:07:09 -0300

    chore: add autocmd to collapse folds on file open

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -11,3 +11,4 @@ end
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.api.nvim_set_keymap("n", "]t", ":tabnext<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "[t", ":tabprevious<CR>", { noremap = true, silent = true })
+vim.cmd([[autocmd BufReadPost,FileReadPost * normal zM]])
