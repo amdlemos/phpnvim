2952ef96c34d5f5f3284dcb6cad9349a2bd260a3
amdlemos <amdlemos@gmail.com>
2025-02-22 00:12:00 -0300

    feat: add plugins -> mason-lspconfig and todo-comments

---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,43 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"eslint",
+					"intelephense",
+					"phpactor",
+					"html",
+					"cssls",
+					"emmet_language_server",
+          "astro",
+          "tailwindcss",
+				},
+			})
+		end,
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = { "stylua", "jq", "xmllint", "prettier" },
+			})
+		end,
+	},
+}
