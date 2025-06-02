612ebecb1b0a373dbdd1799d5ac1f729d5e13c6a
amdlemos <amdlemos@gmail.com>
2025-06-02 06:35:10 -0300

    wip

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -4,8 +4,8 @@ vim.cmd("set termguicolors")
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 
 for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
@@ -13,9 +13,24 @@ vim.api.nvim_set_keymap("n", "]t", ":tabnext<CR>", { noremap = true, silent = tr
 vim.api.nvim_set_keymap("n", "[t", ":tabprevious<CR>", { noremap = true, silent = true })
 vim.cmd([[autocmd BufReadPost,FileReadPost * normal zM]])
 vim.cmd("filetype plugin indent on")
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = "*.ofx",
   callback = function()
     vim.bo.filetype = "xml"
   end,
 })
+
+local diagnostics_active = true
+
+function ToggleDiagnostics()
+  diagnostics_active = not diagnostics_active
+  if diagnostics_active then
+    vim.diagnostic.enable()
+    vim.notify("Diagnostics ON", vim.log.levels.INFO)
+  else
+    vim.diagnostic.disable()
+    vim.notify("Diagnostics OFF", vim.log.levels.WARN)
+  end
+end
+
+vim.keymap.set("n", "<leader>td", ToggleDiagnostics, { desc = "Toggle LSP diagnostics" })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,90 +1,90 @@
 return {
 	{ "onsails/lspkind.nvim" },
-	{
-		"ray-x/lsp_signature.nvim",
-		event = "InsertEnter",
-		opts = {
-			bind = true,
-			handler_opts = {
-				border = "rounded",
-			},
-		},
-		config = function(_, opts)
-			require("lsp_signature").setup(opts)
-		end,
-	},
+	-- {
+	-- 	"ray-x/lsp_signature.nvim",
+	-- 	event = "InsertEnter",
+	-- 	opts = {
+	-- 		bind = true,
+	-- 		handler_opts = {
+	-- 			border = "rounded",
+	-- 		},
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		require("lsp_signature").setup(opts)
+	-- 	end,
+	-- },
 	{
 		"j-hui/fidget.nvim",
 		opts = {
 			-- options
 		},
 	},
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			-- make sure mason installs the server
-			servers = {
-				--- @deprecated -- tsserver renamed to ts_ls but not yet released, so keep this for now
-				--- the proper approach is to check the nvim-lspconfig release version when it's released to determine the server name dynamically
-				tsserver = {
-					enabled = false,
-				},
-				ts_ls = {
-					enabled = true,
-				},
-				vtsls = { enabled = false },
-			},
-		},
-		dependencies = { "saghen/blink.cmp" },
-		debug = true,
-		enabled = true,
-		config = function(_, opts)
-			vim.diagnostic.config({
-				virtual_text = false,
-				signs = true,
-				underline = true,
-				severity_sort = true,
-				update_in_insert = false,
-			})
-
-			local blink_capabilities = require("blink.cmp").get_lsp_capabilities()
-			local lspconfig = require("lspconfig")
-
-			-- default
-			-- require("plugins.lsp.html").setup(lspconfig)
-			-- require("plugins.lsp.css").setup(lspconfig)
-			-- require("plugins.lsp.emmet_language_server").setup(lspconfig)
-			-- require("plugins.lsp.sqlls").setup(lspconfig)
-			-- require("plugins.lsp.sqls").setup(lspconfig)
-
-			-- php
-			-- require("plugins.lsp.intelephense").setup(lspconfig, blink_capabilities)
-			-- require("plugins.lsp.psalm").setup(lspconfig)
-			-- require("plugins.lsp.phan").setup(lspconfig)
-			-- require("plugins.lsp.twiggy_language_server").setup(lspconfig)
-
-			-- ts/js
-			-- require("plugins.lsp.astro").setup(lspconfig, blink_capabilities)
-
-			-- require("plugins.lsp.vtsls").setup(lspconfig, blink_capabilities)
-			-- require("plugins.lsp.ts_ls").setup(lspconfig, blink_capabilities)
-			-- require("plugins.lsp.volar").setup(lspconfig, blink_capabilities)
-			-- require("plugins.lsp.eslint").setup(lspconfig)
-
-			-- ruby
-			-- require("plugins.lsp.ruby_lsp").setup(lspconfig, blink_capabilities)
-			-- require("plugins.lsp.solargraph").setup(lspconfig, blink_capabilities)
-			-- require("plugins.lsp.sorbet").setup(lspconfig, blink_capabilities)
-
-			-- Disabled
-			-- require("plugins.lsp.biome").setup(lspconfig, blink_capabilities)
-			require("plugins.lsp.phpactor").setup(lspconfig, blink_capabilities)
-			-- local lspconfig = require("lspconfig")
-
-			local keymap = vim.keymap
-			local buf = vim.lsp.buf
-
-			keymap.set({ "n", "v" }, "<leader>ca", buf.code_action, { desc = "LSP Code Action" })
-		end,
-	},
+	-- {
+	-- 	"neovim/nvim-lspconfig",
+	-- 	opts = {
+	-- 		-- make sure mason installs the server
+	-- 		servers = {
+	-- 			--- @deprecated -- tsserver renamed to ts_ls but not yet released, so keep this for now
+	-- 			--- the proper approach is to check the nvim-lspconfig release version when it's released to determine the server name dynamically
+	-- 			tsserver = {
+	-- 				enabled = false,
+	-- 			},
+	-- 			ts_ls = {
+	-- 				enabled = true,
+	-- 			},
+	-- 			vtsls = { enabled = false },
+	-- 		},
+	-- 	},
+	-- 	dependencies = { "saghen/blink.cmp" },
+	-- 	debug = true,
+	-- 	enabled = true,
+	-- 	config = function(_, opts)
+	-- 		vim.diagnostic.config({
+	-- 			virtual_text = false,
+	-- 			signs = true,
+	-- 			underline = true,
+	-- 			severity_sort = true,
+	-- 			update_in_insert = false,
+	-- 		})
+	--
+	-- 		local blink_capabilities = require("blink.cmp").get_lsp_capabilities()
+	-- 		local lspconfig = require("lspconfig")
+	--
+	-- 		-- default
+	-- 		-- require("plugins.lsp.html").setup(lspconfig)
+	-- 		-- require("plugins.lsp.css").setup(lspconfig)
+	-- 		-- require("plugins.lsp.emmet_language_server").setup(lspconfig)
+	-- 		-- require("plugins.lsp.sqlls").setup(lspconfig)
+	-- 		-- require("plugins.lsp.sqls").setup(lspconfig)
+	--
+	-- 		-- php
+	-- 		-- require("plugins.lsp.intelephense").setup(lspconfig, blink_capabilities)
+	-- 		-- require("plugins.lsp.psalm").setup(lspconfig)
+	-- 		-- require("plugins.lsp.phan").setup(lspconfig)
+	-- 		-- require("plugins.lsp.twiggy_language_server").setup(lspconfig)
+	--
+	-- 		-- ts/js
+	-- 		-- require("plugins.lsp.astro").setup(lspconfig, blink_capabilities)
+	--
+	-- 		-- require("plugins.lsp.vtsls").setup(lspconfig, blink_capabilities)
+	-- 		-- require("plugins.lsp.ts_ls").setup(lspconfig, blink_capabilities)
+	-- 		-- require("plugins.lsp.volar").setup(lspconfig, blink_capabilities)
+	-- 		-- require("plugins.lsp.eslint").setup(lspconfig)
+	--
+	-- 		-- ruby
+	-- 		-- require("plugins.lsp.ruby_lsp").setup(lspconfig, blink_capabilities)
+	-- 		-- require("plugins.lsp.solargraph").setup(lspconfig, blink_capabilities)
+	-- 		-- require("plugins.lsp.sorbet").setup(lspconfig, blink_capabilities)
+	--
+	-- 		-- Disabled
+	-- 		-- require("plugins.lsp.biome").setup(lspconfig, blink_capabilities)
+	-- 		require("plugins.lsp.phpactor").setup(lspconfig, blink_capabilities)
+	-- 		-- local lspconfig = require("lspconfig")
+	--
+	-- 		local keymap = vim.keymap
+	-- 		local buf = vim.lsp.buf
+	--
+	-- 		keymap.set({ "n", "v" }, "<leader>ca", buf.code_action, { desc = "LSP Code Action" })
+	-- 	end,
+	-- },
 }
