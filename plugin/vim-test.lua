ec9ae53c35fda2250396c86601c8d3cb556c58ab
Alan Lemos <amdlemos@gmail.com>
2025-09-22 18:22:14 -0300

    wip: melhorado comportamento test

--- plugin/vim-test.lua
+++ plugin/vim-test.lua
@@ -2,4 +2,5 @@
 vim.g["test#php#pest#executable"] = "docker compose -f docker-compose.dev.yml exec -T app vendor/bin/pest"
 vim.g["test#strategy"] = "neovim"
 vim.g["test#neovim#term_position"] = "botright"
+-- Não entrar automaticamente em modo insert para permitir navegação
 vim.g["test#neovim#start_insert"] = 0
