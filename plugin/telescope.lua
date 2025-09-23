a6c679b619955d07fc03aa4e99289ab3d3b9286b
Alan Lemos <amdlemos@gmail.com>
2025-09-23 11:47:52 -0300

    wip: lps keymaps

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -61,3 +61,13 @@ vim.keymap.set("n", "<leader>gF", builtin.git_files, { desc = "Git Files (Telesc
 vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git Status (Telescope)" })
 vim.keymap.set("n", "<leader>gC", builtin.git_commits, { desc = "Git Commits (Telescope)" })
 vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "Git Branches (Telescope)" })
+
+-- LSP keymaps com Telescope (melhores que os nativos)
+vim.keymap.set("n", "<leader>l", "", { desc = "LSP" })
+vim.keymap.set("n", "<leader>lr", builtin.lsp_references, { desc = "LSP References (Telescope)" })
+vim.keymap.set("n", "<leader>ld", builtin.lsp_definitions, { desc = "LSP Definitions (Telescope)" })
+vim.keymap.set("n", "<leader>li", builtin.lsp_implementations, { desc = "LSP Implementations (Telescope)" })
+vim.keymap.set("n", "<leader>lt", builtin.lsp_type_definitions, { desc = "LSP Type Definitions (Telescope)" })
+vim.keymap.set("n", "<leader>ls", builtin.lsp_document_symbols, { desc = "LSP Document Symbols (Telescope)" })
+vim.keymap.set("n", "<leader>lw", builtin.lsp_workspace_symbols, { desc = "LSP Workspace Symbols (Telescope)" })
+vim.keymap.set("n", "<leader>lD", builtin.diagnostics, { desc = "LSP Diagnostics (Telescope)" })
