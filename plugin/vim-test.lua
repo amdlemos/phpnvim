cee7cf413e52e210da332611fbf59ee587391c18
Alan Lemos <amdlemos@gmail.com>
2025-09-26 11:07:19 -0300

    wip: vim-test vertical botright

--- plugin/vim-test.lua
+++ plugin/vim-test.lua
@@ -1,6 +1,7 @@
 -- Configuração do vim-test para Docker (sem neotest)
 vim.g["test#php#pest#executable"] = "docker compose -f docker-compose.dev.yml exec -T app vendor/bin/pest"
 vim.g["test#strategy"] = "neovim"
-vim.g["test#neovim#term_position"] = "botright"
+vim.g["test#neovim#term_position"] = "vertical botright" -- Divisão vertical à direita
+vim.g["test#neovim#term_size"] = 80 -- Largura do terminal (80 colunas)
 -- Não entrar automaticamente em modo insert para permitir navegação
 vim.g["test#neovim#start_insert"] = 0
