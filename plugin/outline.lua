1f044538b5b331a4c28ffd9ff20dfc843f6ce6dc
Alan Lemos <amdlemos@gmail.com>
2026-01-24 17:30:51 -0300

    versao com edgy

--- plugin/outline.lua
+++ plugin/outline.lua
@@ -1,19 +1,47 @@
 vim.pack.add({
 	{ src = "https://github.com/hedyhli/outline.nvim" },
 	{ src = "https://github.com/akinsho/toggleterm.nvim" },
+	{ src = "https://github.com/folke/edgy.nvim" },
 }, { load = true })
 
-require("outline").setup({
-	window = {
-		position = "right",
-		width = 40,
-	},
-})
+require("outline").setup({})
 
 require("toggleterm").setup({
-	direction = "horizontal",
 	open_mapping = "<leader>to",
 })
 
--- Example mapping to toggle outline
 vim.keymap.set("n", "<leader>so", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+
+-- edgy.nvim setup for managing window layouts
+require("edgy").setup({
+	animate = {
+		enabled = false,
+	},
+	bottom = {
+		{
+			title = "Terminal",
+			ft = "toggleterm",
+			size = { height = 0.1 },
+		},
+		{
+
+			title = "Trouble",
+			ft = "trouble",
+			size = { height = 0.1 },
+		},
+	},
+	left = {
+		{
+			title = "Neo-Tree",
+			ft = "neo-tree",
+			size = { width = 0.2 },
+		},
+	},
+	right = {
+		{
+			title = "Outline",
+			ft = "Outline",
+			size = { width = 0.15 },
+		},
+	},
+})
