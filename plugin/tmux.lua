a8a9d22f8f93799e0c563a60006269f056f0b1b2
Alan Lemos <amdlemos@gmail.com>
2026-01-24 15:44:49 -0300

    keymaps

--- plugin/tmux.lua
+++ plugin/tmux.lua
@@ -2,14 +2,24 @@
 -- 	return
 -- end
 vim.pack.add({
-    { src = "https://github.com/christoomey/vim-tmux-navigator" },
-}, { load = true})
-local keymap = vim.keymap.set
-local s = { silent = true }
+	{ src = "https://github.com/christoomey/vim-tmux-navigator" },
+}, { load = true })
 
--- Tmux Navigator
-keymap("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", s)
-keymap("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", s)
-keymap("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", s)
-keymap("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", s)
-keymap("n", "<C-\\>", "<cmd>TmuxNavigatePrevious<CR>", s)
+local map = vim.keymap.set
+
+-- normal mode (provavelmente você já tem)
+map("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>")
+map("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>")
+map("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>")
+map("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>")
+
+map("i", "<C-h>", "<Esc><cmd>TmuxNavigateLeft<cr>")
+map("i", "<C-j>", "<Esc><cmd>TmuxNavigateDown<cr>")
+map("i", "<C-k>", "<Esc><cmd>TmuxNavigateUp<cr>")
+map("i", "<C-l>", "<Esc><cmd>TmuxNavigateRight<cr>")
+
+-- terminal mode (ESSENCIAL)
+map("t", "<C-h>", [[<C-\><C-n><cmd>TmuxNavigateLeft<cr>]])
+map("t", "<C-j>", [[<C-\><C-n><cmd>TmuxNavigateDown<cr>]])
+map("t", "<C-k>", [[<C-\><C-n><cmd>TmuxNavigateUp<cr>]])
+map("t", "<C-l>", [[<C-\><C-n><cmd>TmuxNavigateRight<cr>]])
