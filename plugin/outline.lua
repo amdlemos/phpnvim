a2c570fe02be781a0df77d27b27c971a2a5590f1
Alan Lemos <amdlemos@gmail.com>
2026-01-24 21:16:05 -0300

    outline/edgy funcionando bem

--- plugin/outline.lua
+++ plugin/outline.lua
@@ -4,10 +4,16 @@ vim.pack.add({
 	{ src = "https://github.com/folke/edgy.nvim" },
 }, { load = true })
 
-require("outline").setup({})
+require("outline").setup({
+	-- window = {
+	-- 	position = "right",
+	-- 	width = 15,
+	-- },
+})
 
 require("toggleterm").setup({
 	open_mapping = "<leader>to",
+	start_in_insert = false,
 })
 
 vim.keymap.set("n", "<leader>so", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
@@ -29,19 +35,51 @@ require("edgy").setup({
 			ft = "trouble",
 			size = { height = 0.1 },
 		},
+
+		{
+
+			title = "Neotest outpanel",
+			ft = "neotest-output-panel",
+			size = { height = 0.1 },
+		},
 	},
 	left = {
 		{
 			title = "Neo-Tree",
 			ft = "neo-tree",
+			pinned = true, -- ESSENCIAL: Mantém a referência mesmo se fechado
 			size = { width = 0.2 },
 		},
-	},
-	right = {
+
 		{
 			title = "Outline",
 			ft = "Outline",
-			size = { width = 0.15 },
+			pinned = true, -- ESSENCIAL: Mantém a referência mesmo se fechado
+			size = { width = 0.2, height = 0.27 },
+		},
+	},
+	right = {
+
+		{
+			title = "Neotest summary",
+			ft = "neotest-summary",
+			size = { width = 40 },
+		},
+
+		{
+			ft = "dapui_watches",
+			size = { width = 40 },
+		},
+		{ ft = "dap-repl", title = "REPL", size = { height = 10 } },
+		{
+			ft = "dapui_breakpoints",
+			size = { width = 40 },
+
+			size = { width = 0.2, height = 0.27 },
 		},
 	},
 })
+-- Toggle de todo o lado ESQUERDO
+vim.keymap.set("n", "<leader>tl", function()
+	require("edgy").toggle("left")
+end, { desc = "Toggle Edgy Left" })
