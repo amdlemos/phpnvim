f20ed71b5b211e3c9e5bf9fbf5bd7ea9849ef65f
Alan Lemos <amdlemos@gmail.com>
2026-05-27 00:15:49 -0300

    WIP

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -6,3 +6,25 @@ vim.g.dbs = {
 }
 
 vim.keymap.set("n", "<leader>D", "<cmd>DBUIToggle<cr>", { desc = "Abrir DB UI" })
+
+-- Ensure vertical navigation works from DB UI buffers
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "dbui",
+	callback = function()
+	local opts = { buffer = true, silent = true }
+		-- Ctrl-j / Ctrl-Down -> move to window below
+		vim.keymap.set("n", "<C-j>", function()
+			vim.cmd("wincmd j")
+		end, vim.tbl_extend("force", opts, { desc = "DBUI: window below" }))
+
+		-- Ctrl-k / Ctrl-Up -> move to window above
+		vim.keymap.set("n", "<C-k>", function()
+			vim.cmd("wincmd k")
+		end, vim.tbl_extend("force", opts, { desc = "DBUI: window above" }))
+
+		-- Vim-style tree navigation like Neo-tree: l -> child/enter
+		vim.keymap.set("n", "l", "<Plug>(DBUI_GotoChildNode)", vim.tbl_extend("force", opts, { desc = "DBUI: goto child/enter" }))
+		-- h: collapse node if open, otherwise go to parent
+		vim.keymap.set("n", "h", "<Plug>(DBUI_GotoParentNode)", vim.tbl_extend("force", opts, { desc = "DBUI: collapse node" }))
+	end,
+})
