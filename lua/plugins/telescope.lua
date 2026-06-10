cd854f1977b10aecc263dd64bfdebf43dc3cccb7
Alan Lemos <amdlemos@gmail.com>
2026-06-10 11:06:14 -0300

    wip

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -25,7 +25,7 @@ telescope.setup({
 			i = {
 				["<C-j>"] = actions.move_selection_next,
 				["<C-k>"] = actions.move_selection_previous,
-				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
 				["<Esc>"] = actions.close,
 			},
 		},
